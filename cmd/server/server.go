40f2ab66ef83ee1f4275121c37ec50670c6dca22
Jose Berrio <aldarisbm@gmail.com>
2023-05-30 23:54:15 -0400

    added bolt for kv

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	bolt "go.etcd.io/bbolt"
 	"log"
 )
 
@@ -12,4 +13,18 @@ func main() {
 	registerApiV1Routes(apiV1)
 
 	log.Fatal(app.Listen(":3000"))
+}
+
+func createKVStore() (*bolt.DB, error) {
+	dbm, err := bolt.Open(o.path, o.mode, nil)
+	if err != nil {
+		panic(err)
+	}
+	err = dbm.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte(o.bucket))
+		if err != nil {
+			return err
+		}
+		return nil
+	})
 }
